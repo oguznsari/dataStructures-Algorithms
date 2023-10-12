b4135cbeef74338a3e3cde0d3e3002f095dbd3bd
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-10-12 08:57:45 +0300

    Added Tabulation solution for Fibonacci problem

--- 3. Data Structures/memoization.js
+++ 3. Data Structures/memoization.js
@@ -13,6 +13,15 @@ function fibMemo(n, memo = []) {
   return res;
 }
 
+function fibTabulation(n) {
+  if (n <= 2) return 1;
+  var fibNums = [0, 1, 1];
+  for (var i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
+
 performance.mark("start-fib");
 const result1 = fib(35);
 performance.mark("end-fib");
@@ -31,9 +40,16 @@ const duration2 = performance.getEntriesByName(
   "fib-with-memoization-duration"
 )[0].duration;
 
+performance.mark("tabulationStart");
+const result3 = fibTabulation(35);
+performance.mark("tabulationEnd");
+performance.measure("fibTabulation", "tabulationStart", "tabulationEnd");
+const duration3 = performance.getEntriesByName("fibTabulation")[0].duration;
+
 const timingData = {
   "Fibonacci without Memoization": { Result: result1, Duration: duration1 },
   "Fibonacci with Memoization": { Result: result2, Duration: duration2 },
+  "Fibonacci with Tabulation": { Result: result3, Duration: duration3 },
 };
 
 console.table(timingData);
