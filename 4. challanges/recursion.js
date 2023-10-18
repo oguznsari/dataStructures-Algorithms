3b3fa2aece38bae84e08ad5feb85eeb511a097ab
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:33:41 +0300

    recursion | someRecursive

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -47,14 +47,15 @@ console.log(fib(28), "->", 317811); // 317811
 console.log(fib(35), "->", 9227465); // 9227465
 
 // 6
-function reverse(str){
-    if (str.length <= 0) return str;
-    return reverse(str.slice(1)) + str[0];
+function reverse(str) {
+  if (str.length <= 0) return str;
+  return reverse(str.slice(1)) + str[0];
 }
-  
+
 console.log(reverse("awesome"), "->", "emosewa");
 console.log(reverse("rithmschool"), "->", "loohcsmhtir");
 
+// 7
 function isPalindrome(str) {
   if (str.length === 1) return true;
   if (str.length === 2) return str[0] === str[1];
@@ -67,3 +68,19 @@ console.log(isPalindrome("foobar"), "->", false);
 console.log(isPalindrome("tacocat"), "->", true);
 console.log(isPalindrome("amanaplanacanalpanama"), "->", true);
 console.log(isPalindrome("amanaplanacanalpandemonium"), "->", false);
+
+// 8
+const isOdd = (val) => val % 2 !== 0;
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+console.log(someRecursive([1, 2, 3, 4], isOdd), "->", true);
+console.log(someRecursive([4, 6, 8, 9], isOdd), "->", true);
+console.log(someRecursive([4, 6, 8], isOdd), "->", false);
+console.log(
+  someRecursive([4, 6, 8], (val) => val > 10),
+  "->",
+  false
+);
