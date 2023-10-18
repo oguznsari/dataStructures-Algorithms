6c770c1134fc62ab0d9beff69529b1e76849dd7d
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:28:29 +0300

    recursion | isPalindrome

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -52,5 +52,18 @@ function reverse(str){
     return reverse(str.slice(1)) + str[0];
 }
   
-console.log(reverse('awesome'), "->", 'emosewa')
-console.log(reverse('rithmschool'), "->", 'loohcsmhtir')
+console.log(reverse("awesome"), "->", "emosewa");
+console.log(reverse("rithmschool"), "->", "loohcsmhtir");
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+console.log(isPalindrome("awesome"), "->", false);
+console.log(isPalindrome("foobar"), "->", false);
+console.log(isPalindrome("tacocat"), "->", true);
+console.log(isPalindrome("amanaplanacanalpanama"), "->", true);
+console.log(isPalindrome("amanaplanacanalpandemonium"), "->", false);
