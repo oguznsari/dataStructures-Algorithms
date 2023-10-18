9f0c049d779e3a398abf52b99de2a48aa0e53fbd
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:50:37 +0300

    recursion | CAPITALIZEWORDS

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -156,3 +156,16 @@ var obj2 = {
 
 console.log(nestedEvenSum(obj1), "->", 6);
 console.log(nestedEvenSum(obj2), "->", 10);
+
+// 12
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
+
+let words = ["i", "am", "learning", "recursion"];
+console.log(capitalizeWords(words), "->", ["I", "AM", "LEARNING", "RECURSION"]);
