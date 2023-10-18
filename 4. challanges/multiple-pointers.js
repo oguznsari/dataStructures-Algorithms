9070ecfef14956c7dc78349de767a9a5f80748f7
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 19:14:00 +0300

    multiple pointers 2

--- 4. challanges/multiple-pointers.js
+++ 4. challanges/multiple-pointers.js
@@ -17,7 +17,27 @@ function averagePair(arr, target) {
   return false;
 }
 
-console.log(averagePair([1, 2, 3], 2.5), true);
-console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8), true);
-console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1), false);
-console.log(averagePair([], 4), false);
+console.log(averagePair([1, 2, 3], 2.5), "->", true);
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8), "->", true);
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1), "->", false);
+console.log(averagePair([], 4), "->", false);
+
+// 2nd
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+
+  return false;
+}
+
+console.log(isSubsequence("sing", "sting"), "->", true); // true
+console.log(isSubsequence("abc", "abracadabra"), "->", true); // true
+console.log(isSubsequence("abc", "acb"), "->", false); // false (order matters)
+console.log(isSubsequence("hello", "hello world"), "->", true); // true
