4522c31672a9fe9464ab5f67c3b72cb90c530d43
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 19:55:26 +0300

    sliding window 3 | findLongestSubstring

--- 4. challanges/sliding-window.js
+++ 4. challanges/sliding-window.js
@@ -22,34 +22,64 @@ console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2), "->", 5); // 5
 console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2), "->", 5); // 5
 console.log(maxSubarraySum([2, 3], 3), "->", null); // null
 
-
-// 2 
+// 2
 function minSubArrayLen(arr, num) {
-    let total = 0;
-    let start = 0;
-    let end = 0;
-    let minLen = Infinity;
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
 
-    while (start < arr.length) {
-        if (total < num && end < arr.length) {
-            total += arr[end];
-            end++;
-        } else if (total >= num) {
-            minLen = Math.min(minLen, end - start); 
-            total -= arr[start];
-            start++;
-        } else {
-            break;
-        }
+  while (start < arr.length) {
+    if (total < num && end < arr.length) {
+      total += arr[end];
+      end++;
+    } else if (total >= num) {
+      minLen = Math.min(minLen, end - start);
+      total -= arr[start];
+      start++;
+    } else {
+      break;
     }
+  }
 
-    return minLen === Infinity ? 0 : minLen;
+  return minLen === Infinity ? 0 : minLen;
 }
 
-console.log(minSubArrayLen([2,3,1,2,4,3], 7), "->", 2) // 2 -> because [4,3] is the smallest subarray
-console.log(minSubArrayLen([2,1,6,5,4], 9), "->", 2) // 2 -> because [5,4] is the smallest subarray
-console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52), "->", 1) // 1 -> because [62] is greater than 52
-console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],39), "->", 3) // 3
-console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],55), "->", 5) // 5
-console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11), "->", 2) // 2
-console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10],95), "->", 0) // 0
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7), "->", 2); // 2 -> because [4,3] is the smallest subarray
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9), "->", 2); // 2 -> because [5,4] is the smallest subarray
+console.log(
+  minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52),
+  "->",
+  1
+); // 1 -> because [62] is greater than 52
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39), "->", 3); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55), "->", 5); // 5
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11), "->", 2); // 2
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95), "->", 0); // 0
+
+// 3
+
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+console.log(findLongestSubstring(""), "->", 0);
+console.log(findLongestSubstring("rithmschool"), "->", 7);
+console.log(findLongestSubstring("thisisawesome"), "->", 6);
+console.log(findLongestSubstring("thecatinthehat"), "->", 7);
+console.log(findLongestSubstring("bbbbbb"), "->", 1);
+console.log(findLongestSubstring("longestsubstring"), "->", 8);
+console.log(findLongestSubstring("thisishowwedoit"), "->", 6);
