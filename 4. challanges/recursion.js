ebc1b37a93c619b5b8f38afbbf94575c41c80c37
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:04:34 +0300

    recursion | factorial!

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -6,3 +6,14 @@ function power(base, pow) {
 console.log(power(2, 0), "->", 1);
 console.log(power(2, 2), "->", 4);
 console.log(power(2, 4), "->", 16);
+
+// 2
+function factorial(num) {
+    if (num < 2) return 1;
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(1), "->", 1); // 1
+console.log(factorial(2), "->", 2); // 2
+console.log(factorial(4), "->", 24); // 24
+console.log(factorial(7), "->", 5040); // 5040
