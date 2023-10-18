e6a760228513486ee62842cef34c7e079e9bf03c
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:23:59 +0300

    recursion | reverse string

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -45,3 +45,12 @@ console.log(fib(4), "->", 3); // 3
 console.log(fib(10), "->", 55); // 55
 console.log(fib(28), "->", 317811); // 317811
 console.log(fib(35), "->", 9227465); // 9227465
+
+// 6
+function reverse(str){
+    if (str.length <= 0) return str;
+    return reverse(str.slice(1)) + str[0];
+}
+  
+console.log(reverse('awesome'), "->", 'emosewa')
+console.log(reverse('rithmschool'), "->", 'loohcsmhtir')
