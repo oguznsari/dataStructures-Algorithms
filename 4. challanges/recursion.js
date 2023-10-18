477cc9c2fc26f1c4418463275d624cc0e50bea98
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:08:48 +0300

    recursion | productOfArray

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -17,3 +17,11 @@ console.log(factorial(1), "->", 1); // 1
 console.log(factorial(2), "->", 2); // 2
 console.log(factorial(4), "->", 24); // 24
 console.log(factorial(7), "->", 5040); // 5040
+
+// 3
+function productOfArray(arr) {
+    if (arr.length === 1) return arr[0];
+    return arr[arr.length -1] * productOfArray(arr.slice(0, arr.length -1)) 
+}
+console.log(productOfArray([1,2,3]), "->", 6);
+console.log(productOfArray([1,2,3,10]), "->", 60);
