e998fc8e4748a886f18272f35dfa209fbba8a078
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:53:01 +0300

    recursion | stringifyNumbers

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -169,3 +169,32 @@ function capitalizeWords(array) {
 
 let words = ["i", "am", "learning", "recursion"];
 console.log(capitalizeWords(words), "->", ["I", "AM", "LEARNING", "RECURSION"]);
+
+// 13
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
+
+console.log(stringifyNumbers(obj), "->", "numbers stringified in obj");
