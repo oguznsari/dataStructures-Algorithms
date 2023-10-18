1a01f02bee57654270dc1a2224366c16865f8570
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-10-18 20:37:05 +0300

    recursion | flattenArray

--- 4. challanges/recursion.js
+++ 4. challanges/recursion.js
@@ -84,3 +84,21 @@ console.log(
   "->",
   false
 );
+
+// 9
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5]]), "->", [1, 2, 3, 4, 5]);
+console.log(flatten([1, [2, [3, 4], [[5]]]]), "->", [1, 2, 3, 4, 5]);
+console.log(flatten([[1], [2], [3]]), "->", [1, 2, 3]);
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]), "->", [1, 2, 3]);
