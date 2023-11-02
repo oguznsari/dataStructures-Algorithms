862b940fb6874c2cf7dd6526bf930d7c05a99b0c
OĞUZHAN <oguzhan.sari@orioninc.com>
2023-11-02 22:39:23 +0300

    fixed naming error

--- 3. Data Structures/singly-linked-list.js
+++ 3. Data Structures/singly-linked-list.js
@@ -114,7 +114,7 @@ class SinglyLinkedList {
     if (index < 0 || index > this.length) return false;
     if (index === 0) return this.shift();
     if (index === this.length - 1) return this.pop();
-    var prev = this.get(index - 1);
+    var previousNode = this.get(index - 1);
     var removed = previousNode.next;
     previousNode.next = removed.next;
     this.length--;
